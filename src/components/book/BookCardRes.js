bf9c81410c7039f50875e4b757bb31c93cc06b31
irensvm <irens.vm@gmail.com>
2020-09-16 16:21:13 +0200

    heroku

--- src/components/book/BookCardRes.js
+++ src/components/book/BookCardRes.js
@@ -3,40 +3,42 @@ import { Card, CardColumns, CardDeck } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import FavButton from "./FavButton";
 import AddBookButton from "./AddBookButton";
-
+import "./BookCardRes.css";
 export default function BookCard(props) {
   return (
-    <div
-      className="card-res"
-      style={{ marginTop: 20, marginBottom: 20, width: 900, height: 720 }}
-    >
+    <div className="card-res">
       <CardDeck className="card m-2 wid">
         <Card bg="dark" text="light" border="light" className="book-card">
-          <Card.Img
-            style={{ width: "50%" }}
-            variant="top"
-            src={props.image}
-            alt="book.img"
-          />
-          <Card.Title text="light" className="card-text">
+          <div className="book-card-title">
+            <Card.Title text="light" className="card-text">
+              {" "}
+              {props.title}
+            </Card.Title>
+          </div>
+          <div className="book-card-left">
             {" "}
-            {props.title}
-          </Card.Title>
-          <Card.Text
-            text="light"
-            className="card-text"
-            line={1}
-            style={{ flex: 1, flexWrap: "wrap", margin: 10 }}
-          >
-            {props.description}
-          </Card.Text>
-          <Card.Body>
-            <FavButton></FavButton>
-            <AddBookButton></AddBookButton>
-          </Card.Body>
-          <Card.Footer text="light">
-            <small className="text-muted">Written by: {props.author}</small>
-          </Card.Footer>
+            <Card.Img variant="top" src={props.image} alt="book.img" />
+          </div>
+          <div className="book-card-right">
+            {" "}
+            <Card.Text
+              text="light"
+              className="card-text"
+              line={1}
+              style={{ flex: 1, flexWrap: "wrap", margin: 10 }}
+            >
+              {props.description}
+            </Card.Text>
+            <Card.Body>
+              <FavButton></FavButton>
+              <AddBookButton></AddBookButton>
+            </Card.Body>
+          </div>
+          <div className="book-card-author">
+            <Card.Footer text="light">
+              <small>By: {props.author}</small>
+            </Card.Footer>
+          </div>
         </Card>
       </CardDeck>
     </div>
